51315b0f1bfef83bb3861b53f22c0a19dab3f38d
Michat <michael.gungaram@gmail.com>
2018-06-25 18:48:12 +0200

    Soft Skills beginning.

--- src/components/Skills/Autonomy/AutonomyView.vue
+++ src/components/Skills/Autonomy/AutonomyView.vue
@@ -1,17 +1,24 @@
 <template>
-    <div class="item">
-        <div class="image">
-                
-        </div>
-        <div class="content">
-            <a class="header"></a>
-            <div class="meta">
-
-            </div>
-            <div class="description">
-
-            </div>
-        </div>
+    <div class="ui raised segment">
+        <h2>L'autonomie</h2>
+        <p>
+            L'autonomie est importante pour être efficace et pour pouvoir se débrouiller au quotidien. Elle implique cependant de la liberté d'action. 
+            
+            <h4>L'autonomie en entreprise</h4>
+            Je n'ai évolué que dans de petites structures. Or dans ce type d'entreprise, les individus sont 
+            très prisés par leur travail. Il m'est arrivé parfois de me retrouver en totale autonomie sur un projet, voir même à diriger un binôme 
+            de développeurs juniors. Ce fut difficile au début d'être manager, mais finalement, il semble que les développeurs furent plutôt content 
+            du travail, j'en ai donc déduis qu'ils avaient plutôt bien vécu l'expérience et que tout avait été relativement bien managé. J'ai dû 
+            développer une certaine autonomie dans le développement de compétences techniques et de gestion. Mais aujourd'hui cela me permet d'être indépendant 
+            dans mes choix techniques et dans mes prises de décision.
+            <h4>L'autonomie... Mais attention à l'excès</h4>
+            <p>
+            En revanche, si j'ai un doute, je n'hésite pas à demander conseil envers quelqu'un de plus 
+            expérimenté que moi, car il est important de rester humble et de toujours se remettre en question, surtout dans notre métier. Sur le projet ROC, je 
+            me suis retrouvé en totale autonomie et donc j'ai pu être indépendant en concevant de A à Z la stack de ce projet, qui furent Angular et ASP .NET C#.
+            J'aurais développé l'autonomie tout au long de mes deux dernières années en entreprise. Je n'ai pas vraiment eu le choix que de l'avoir de suite, au moment présent. 
+            </p>
+        </p>
     </div>
 </template>
 <script>
@@ -20,5 +27,7 @@
     }
 </script>
 <style scoped>
-
+    .ui.raised.segment {
+        font-size: large;
+    }
 </style>
--- src/components/Skills/Communication/CommunicationView.vue
+++ src/components/Skills/Communication/CommunicationView.vue
@@ -1,17 +1,23 @@
 <template>
-    <div class="item">
-        <div class="image">
-                
-        </div>
-        <div class="content">
-            <a class="header"></a>
-            <div class="meta">
-
-            </div>
-            <div class="description">
-
-            </div>
-        </div>
+    <div class="ui raised segment">
+        <h2>Communication et esprit d'équipe</h2>
+        <h4>Ma vision</h4>
+        <p>
+            La communication, ce n'est pas juste parler. C'est se comprendre et comprendre les enjeux de chacun.
+        </p>
+        <h4>Dans le contexte professionnel</h4>
+        <p>
+            Même si j'ai beaucoup été seul et que cela m'a permis de développer de l'autonomie et de l'indépendance, j'ai pu aussi travailler en équipe, 
+            notamment avec ma dernière expérience en date, BeMyBee. La coordination de plusieurs développeurs, la communication avec le développeur Back furent primordiaux pour le bon déroulement du projet ainsi que la synchronisation avec tout le monde. Un bon travail d'équipe résulte de la réussite d'un projet ou non. 
+            Il faut pouvoir travailler ensemble, peu importe les méthodes, que l'on soit à côté ou non ; les technologies aujourd'hui nous offrent 
+            l'opportunité de travailler en équipe même si nous ne sommes pas tous à proximité ; Skype, Slack, de tels outils nous permettent cela. 
+            C'est comme ceci que j'ai dû parfois opérer. 
+        </p>
+        <h4>La communication a toujours été</h4>
+        <p>
+            La communication est assez innée chez moi et je n'ai pas eu besoin d'expériences en entreprise pour la développer ; c'est la vie qui me 
+            l'a enseigné. L'esprit d'équipe par contre, j'ai pu l'acquérir au fil des projets, par exemple avec JLL et BeMyBee, ou encore l'infrastructure de tests d'API.
+        </p>
     </div>
 </template>
 <script>
@@ -20,5 +26,7 @@
     }
 </script>
 <style scoped>
-
+    .ui.raised.segment {
+        font-size: large;
+    }
 </style>
