2a3be17be03f69d71990ae624efb7c3bb8efc7e9
Michat <michael.gungaram@gmail.com>
2018-06-25 19:32:08 +0200

    Soft skills done.

--- src/components/Skills/Empathy/EmpathyView.vue
+++ src/components/Skills/Empathy/EmpathyView.vue
@@ -1,17 +1,31 @@
 <template>
-    <div class="item">
-        <div class="image">
-                
-        </div>
-        <div class="content">
-            <a class="header"></a>
-            <div class="meta">
-
-            </div>
-            <div class="description">
-
-            </div>
-        </div>
+    <div class="ui raised segment">
+        <h2>Écoute et empathie</h2>
+        <h4>Ma vision</h4>
+        <p>
+            Quand on travaille avec un client, il est important de se mettre à sa place et 
+            de comprendre sa demande. Il est important de ne mettre aucune touche d'ego dans 
+            nos créations, c'est à coup sûr le meilleur moyen de ne répondre à aucune demande. Il faut savoir écouter, et faire preuve de patience. Mais dans tout cela, il faut aussi être honnête et parfois dire non, pour le bien du projet.
+            C'est je pense, la règle principale de tout bon professionnel. 
+        </p>
+        <h4>Et en entreprise ?</h4>
+        <p>
+            S'il y a bien quelque chose que j'ai toujours su faire, c'est l'écoute et avoir du contact humain. 
+            Le contexte clientèle n'est pas différent car nous sommes tous des êtres humains. Certains doivent 
+            l'acquérir au fil de leur vie ; je dois avouer que pour ma part, c'est quelque chose qui a 
+            toujours fait partie intégrante de ma personnalité.
+        </p>
+        <p>
+            Comme je le disais, l'ingénierie logicielle est une affaire humaine ; savoir 
+            écouter est un élément clé pour réussir un projet. Je ne travaille pas dans un rapport de force 
+            vis-à-vis de mon client, j'écoute et je travaille avec lui. En l'écoutant je lui offre ma garantie 
+            de satisfaire sa demande, de ce fait, la qualité du développement est meilleur car tout est ciblé ; 
+            le client est content, il revient vers nous, et ainsi de suite. Voilà ce que l'écoute peut avoir 
+            comme cercle vertueux, à mon sens.  Un bon professionnel est un professionnel qui sait 
+            se mettre à la place de son client. On ne code pas pour soi - sauf un projet personnel, 
+            entendons-nous bien, et encore, la finalité c'est un produit qui va servir aux autres dans 
+            la plupart des cas -, on code pour autrui.
+        </p>
     </div>
 </template>
 <script>
@@ -20,5 +34,7 @@
     }
 </script>
 <style scoped>
-
+    .ui.raised.segment {
+        font-size: large;
+    }
 </style>
--- src/components/Skills/TechWatching/TechWatchingView.vue
+++ src/components/Skills/TechWatching/TechWatchingView.vue
@@ -1,17 +1,45 @@
 <template>
-    <div class="item">
-        <div class="image">
-                
-        </div>
-        <div class="content">
-            <a class="header"></a>
-            <div class="meta">
-
-            </div>
-            <div class="description">
-
-            </div>
-        </div>
+    <div class="ui raised segment">
+        <h2>Veille technologique et amélioration continue</h2>
+        <h4>Contexte</h4>
+        <p>
+            Le monde du développement logiciel est un monde qui évolue vite, très vite. 
+            Parfois trop vite. On doit toujours rester à la page, frais techniquement 
+            pour garantir un logiciel d'une meilleure qualité possible. 
+            Tout comme le chirurgien qui se tient au courant des dernières avancées scientifiques 
+            pour sans cesse innover, nous, les programmeurs, faisons pareil. Après tout, l'informatique 
+            est un domaine scientifique.
+        </p>
+        <h4>À la rencontre d'autres savoirs...</h4>
+        <p>
+            J'essaie, pour ma part, d'assister dans la mesure du possible à des conférences 
+            sur différents sujets liés à l'ingénierie logicielle, ou bien ma spécialité. 
+            J'aime beaucoup suivre aussi des auteurs, personnalités influentes dans 
+            le monde de l'informatique : leurs écrits m'inspirent, leur technique me 
+            fascine et me pousse à me dépasser à chaque fois. J'admire ces auteurs qui 
+            savent transmettre leurs connaissances à travers les écrits de leurs livres. 
+            En général, quand on lit un livre d'un certain auteur, on s'oriente vers des 
+            livres du même niveau d'excellence, ce qui garantit un certain niveau technique. 
+            Je pense par exemple à ces éditeurs tels que O'Reilly, Manning, spécialisés dans 
+            l'informatique, dont les livres m'ont aidé et m'aident énormément dans l'acquisition de savoirs. 
+        </p>
+        <h4>La communauté m'inspire beaucoup</h4>
+        <p>
+            Par ailleurs, je suis aussi différents blogs de personnes qui sont à l'origine de différents 
+            frameworks de référence aujourd'hui, ou bien travaillant ou ayant travaillé dans des entreprises
+            assez cotées, comme Google, Microsoft, Yahoo!, ou encore d'autres sociétés ou laboratoires 
+            informatiques qui gagnent tout aussi bien à être connus de par les individus qui la composent.
+            Mais en général, ces sociétés ont des problématiques très intéressantes en ce qui concerne 
+            l'ingénierie logicielle car ils brassent énormément de lignes de code, des quantités astronomiques 
+            de données… Et donc partagent les solutions à différents problèmes, leur façon de gérer leur usine 
+            logicielle, leurs bonnes pratiques… Qui sont loin d'être bêtes. Dans 99% des cas, cela nous aide, 
+            nous, programmeurs à plus petite échelle. C'est cette force de communauté qui fait que j'aime ce métier 
+            ; ce partage de savoir humble, riche et sans prétention.  En général, ce sont des personnes brillantes, 
+            et je suis admiratif de leur humilité, et de leur ouverture d'esprit à partager des connaissances 
+            techniques, et leur façon d'appréhender différents problèmes de performance, leur contribution à la 
+            communauté avec l'open source… Je remercie vaillamment ces personnes, ces contributeurs, sans qui certains
+             projets, certains business, ne tourneraient en aucun cas aujourd'hui.
+        </p>
     </div>
 </template>
 <script>
@@ -20,5 +48,7 @@
     }
 </script>
 <style scoped>
-
+    .ui.raised.segment {
+        font-size: large;
+    }
 </style>
